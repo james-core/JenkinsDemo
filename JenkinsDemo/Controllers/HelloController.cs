06a189dda2735af23a66012014802fc585b0465e
zhengpeng <zhengpeng@7net.cc>
2021-09-03 09:45:55 +0800

    jenkinds 0903

---
+++ JenkinsDemo/Controllers/HelloController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        /// <summary>
+        /// Get方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public List<string> Get()
+        {
+            return new List<string>(){ "this is get"};
+        }
+
+        /// <summary>
+        /// Post方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public List<string> Post()
+        {
+            return new List<string>() { "this is post" };
+        }
+
+        /// <summary>
+        /// Put方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        public List<string> Put()
+        {
+            return new List<string>() { "this is put" };
+        }
+
+        /// <summary>
+        /// Delete方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete]
+        public List<string> Delete()
+        {
+            return new List<string>() { "this is delete" };
+        }
+    }
+}
---
+++ JenkinsDemo/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // 添加Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "JenkinsDemo 接口文档",
+                    Description = $"JenkinsDemo HTTP API v1"
+                });
+            });
+
+            services.AddControllers();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // 添加Swagger有关中间件
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "JenkinsDemo v1");
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
